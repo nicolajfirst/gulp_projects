b72ae38b20ed6b7fd9cb82ef7642fe5e38c5cefa
Nick <nicolajfirst@gmail.com>
2018-08-19 23:08:16 +0300

    add anamation

--- 09_creative-decision/js/main.js
+++ 09_creative-decision/js/main.js
@@ -9,4 +9,23 @@ $(document).ready(function(){
   	cssEase: 'ease-in'
   });
 
+
+// Animation on scroll
+
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated bounceIn');	
+	});
+
+	$('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated rotateInUpLeft');	
+	});
+
+	$('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-3').addClass('animated slideInUp');	
+	});
+
+	$('.js--wp-4').waypoint(function(direction) {
+		$('.js--wp-4').addClass('animated bounceIn');	
+	});
+
 });
