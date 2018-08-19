3e9ed037b7d122fea7429468a5d2bb9cfbcfd3f4
Nick <nicolajfirst@gmail.com>
2018-08-19 19:50:02 +0300

    fontawesome + footer

---
+++ 09_creative-decision/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  $('#js--slider').slick({
+  	autoplay: true,
+  	dots: true,
+  	infinite: true,
+  	speed: 500,
+  	fade: true,
+  	cssEase: 'ease-in'
+  });
+
+});
